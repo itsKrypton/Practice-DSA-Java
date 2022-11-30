bed1a3fab915ffadb708abe3f80ea407827f5893
Saurav <saurav.dutta72@yahoo.com>
2022-11-30 14:10:07 +0530

    update

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -10,9 +10,11 @@ public class BalancedBinaryTree {
         return 0;
 
         int left = maxDepth(root.left);
+        if(left == -1) return -1;
         int right = maxDepth(root.right);
+        if(right == -1) return -1;
 
-        if(Math.abs(left - right) > 1 || left == -1 || right == -1)
+        if(Math.abs(left - right) > 1)
         return -1;
 
         return 1 + Math.max(left, right);
