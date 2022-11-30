1499d0d49b7e049abd68976dd74c1bb702e32b10
Saurav <saurav.dutta72@yahoo.com>
2022-11-30 10:38:14 +0530

    update

--- BinaryTreePostorderTraversal.java
+++ BinaryTreePostorderTraversal.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -9,7 +10,8 @@ public class BinaryTreePostorderTraversal {
         return ans;
     }
 
-    public static void find(TreeNode root, List<Integer> ans)
+    // Recursive Approach
+    /* public static void find(TreeNode root, List<Integer> ans)
     {
         if(root == null)
         return;
@@ -17,6 +19,34 @@ public class BinaryTreePostorderTraversal {
         find(root.left, ans);
         find(root.right, ans);
         ans.add(root.val);
+    } */
+
+    //Tabulated using 2 stacks T: O(2n) S: O(2n)
+    public static void find(TreeNode root, List<Integer> ans)
+    {
+        if(root == null)
+        return;
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+        while(!stack1.isEmpty())
+        {
+            TreeNode currentNode = stack1.pop();
+            stack2.push(currentNode); // You can also directly add it to the ans list and reverse it at the end if you don't want to use second stack.
+
+            if(currentNode.left != null)
+            stack1.push(currentNode.left);
+
+            if(currentNode.right != null)
+            stack1.push(currentNode.right);
+        }
+
+        while(!stack2.isEmpty())
+        ans.add(stack2.pop().val);
+
+        return;
     }
 
     public class TreeNode {
