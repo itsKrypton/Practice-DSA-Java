b9b61d2317733ce94998a0846b4f89730f6b2815
Saurav <saurav.dutta72@yahoo.com>
2023-07-22 13:32:23 +0530

    update

--- FindEventualSafeStates.java
+++ FindEventualSafeStates.java
@@ -6,7 +6,10 @@
  */
 
 import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.LinkedList;
 import java.util.List;
+//import java.util.Queue;
 
 public class FindEventualSafeStates {
     public List<Integer> eventualSafeNodes(int[][] graph) {
@@ -46,4 +49,53 @@ public class FindEventualSafeStates {
         pathVisited[currVertex] = false;
         return false;
     }
+
+    // Using Kahns algo (Topo sort)
+    /*
+     * As we need nodes reaching terminal nodes which have outgoing nodes as 0. We reverse the entire graph so the terminal nodes will now have incoming nodes as 0.
+     * We can then just add all the nodes connected to the terminal node and not forming a cycle using Kahn's algo. Reverse the ans and return it.
+     */
+    /* public List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        List<List<Integer>> revAdj = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++)
+        revAdj.add(new ArrayList<>());
+
+        for(int i = 0; i < V; i++)
+        {
+            for(int node : adj.get(i))
+            revAdj.get(node).add(i);
+        }
+
+        int[] indegree = new int[V];
+        for(List<Integer> list : revAdj)
+        {
+            for(int node : list)
+            indegree[node]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < V; i++)
+        {
+            if(indegree[i] == 0)
+            queue.add(i);
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        while(!queue.isEmpty())
+        {
+            int currentNode = queue.remove();
+            ans.add(currentNode);
+            
+            for(int nVertex : revAdj.get(currentNode))
+            {
+                if(--indegree[nVertex] == 0)
+                queue.add(nVertex);
+            }
+        }
+        
+        Collections.sort(ans);
+        return ans;
+    } */
 }
