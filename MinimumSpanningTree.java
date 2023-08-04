f9aebb38c5a38ae40dd23d8a63784ea8c8f70a72
Saurav <saurav.dutta72@yahoo.com>
2023-08-04 13:48:52 +0530

    update

--- MinimumSpanningTree.java
+++ MinimumSpanningTree.java
@@ -1,18 +1,24 @@
 // https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+//import java.util.ArrayList;
+import java.util.Arrays;
+//import java.util.PriorityQueue;
 
 /*
- * Used to find MST, take a visited node and priority queue which will store neighbor nodes and weights. For Prims algo, start with any node and store all it's 
+ * Prims: Used to find MST, take a visited node and priority queue which will store neighbor nodes and weights. For Prims algo, start with any node and store all it's 
  * unvisited edges to a priority queue, take the edge with the smallest weight and mark it as visited. Continue till the queue is not empty. 
- * 
  * T: ElogE
+ * S: V+E
+ * 
+ * Kruskals: Sort the given edges in ascending order. Use a disjointset (see it's explanation in DisjointSet) to find the ultimate parent of the two given nodes,
+ * if it's not the same then consider that edge. If the parents come out to be same that means that it will form a cycle and we don't want that in MST.
+ * T: E
+ * S: V (In DisjointSet)
  */
 
 public class MinimumSpanningTree {
     // Don't need parent node in Tuple for this question as it doesnt ask for MST edges, just the sum, have just added it to understand how to get mst edges as well.
-    static int spanningTree(int V, int E, int edges[][]){
+    /* static int spanningTree(int V, int E, int edges[][]){
 	    ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
 	    //ArrayList<int[]> mst = new ArrayList<>();
 	    boolean[] visited = new boolean[V];
@@ -66,5 +72,74 @@ public class MinimumSpanningTree {
 	        this.node = node;
 	        this.parent = parent;
 	    }
+	} */
+
+	static int spanningTree(int V, int E, int edges[][]){
+	    //ArrayList<int[]> mst = new ArrayList<>();
+	    DisjointSet ds = new DisjointSet(V);
+	    Arrays.sort(edges, (a,b) -> Integer.compare(a[2], b[2]));
+	    int sum = 0;
+	    
+	    for(int[] edge : edges)
+	    {
+	        if(ds.findParent(edge[0]) != ds.findParent(edge[1]))
+	        {
+	            //mst.add(new int[]{edge[0], edge[1]});
+	            ds.unionBySize(edge[0], edge[1]);
+	            sum += edge[2];
+	        }
+	    }
+	    
+	    return sum;
 	}
+	
+	public static class DisjointSet {
+        private int[] size;
+        private int[] parent;
+    
+        public DisjointSet(int length)
+        {
+            this.size = new int[length + 1];
+            this.parent = new int[length + 1];
+    
+            for(int i = 0; i <= length; i++)
+            {
+                size[i] = 1;
+                parent[i] = i;
+            }
+        }
+    
+        // Finds ultimate parent of the given vertex in O(4 alpha) ~ O(1) uses path compression to get this
+        public int findParent(int vertex)
+        {
+            if(this.parent[vertex] == vertex)
+            return vertex;
+    
+            return parent[vertex] = findParent(this.parent[vertex]);
+        }
+    
+        // Connects the parent of node u and node v depending on their ranks/size in O(4 alpha) ~ O(1)
+        // Union By Size is more intuitive so use this instead of rank.
+        public void unionBySize(int u, int v)
+        {
+            int parentOfU = findParent(u);
+            int parentOfV = findParent(v);
+    
+            // We only perform union if parents are different, if they're same then no need.
+            if(parentOfU != parentOfV)
+            {
+                if(size[parentOfU] < size[parentOfV])
+                {
+                    parent[parentOfU] = parentOfV;
+                    size[parentOfV] += size[parentOfU];
+                }
+    
+                else
+                {
+                    parent[parentOfV] = parentOfU;
+                    size[parentOfU] += size[parentOfV];
+                }
+            }
+        }
+    }
 }
